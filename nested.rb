f734ef188a2a600d5f42df46e8aab8054430974d
Ben Miriello <benmiriello@gmail.com>
2019-11-05 16:02:59 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -2,22 +2,27 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {
+ 		     #:yukihiro_matsumoto => {
+        #   :known_for => "Ruby",
+        #   :languages => ["LISP", "C"]
+        # },
+         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+         },
+         :alan_kay => {
+           :known_for => "Object Orientation",
+           :languages => ["Smalltalk", "LISP"]
+         },
+         :dennis_ritchie => {
+           :known_for => "Unix",
+           :languages => ["C"]
+         }
      }
-
-
+  programmer_hash[:grace_hopper]
+#looking for {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +42,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,13 +61,15 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].join
+
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#     :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
@@ -80,7 +88,16 @@ def adding_matz
         }
      }
 
-    
+  hash_2 =
+  {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+  }
+
+  programmer_hash.merge(hash_2)
+
 end
 
 def changing_alan
@@ -101,8 +118,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+    
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +140,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash
 end
