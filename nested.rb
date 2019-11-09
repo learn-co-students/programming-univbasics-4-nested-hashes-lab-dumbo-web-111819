9d429ee3b411d132fcda5b56e341777b54349a9d
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-09 05:44:58 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -16,12 +15,10 @@ def hopper
         }
      }
   programmer_hash[:grace_hopper]
-
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	
 	programmer_hash = 
  		{
@@ -38,7 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay][:known_for]
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -57,7 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages].join
+     programmer_hash[:dennis_ritchie][:languages].join('')
 end
 
 def adding_matz
@@ -82,9 +79,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-  programmer_hash
-    
+
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    programmer_hash
 end
 
 def changing_alan
@@ -126,6 +125,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
-  programmer_hash
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
+
 end
